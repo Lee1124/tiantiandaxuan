b90886a96f1b34a9756dc95ac61d4ce7dc311cc8
Lee1124 <526551383@qq.com>
2020-02-23 19:51:43 +0800

    活动

--- release/web/js/bundle.js
+++ release/web/js/bundle.js
@@ -233,25 +233,32 @@
     class Main {
         constructor() {
             //是不是登录才进来
-            this.isLoginFrom=false;
-            this.AUTONUM=0;
-            this.AUTO=false;
+            this.isLoginFrom = false;
+            this.AUTONUM = 0;
+            this.AUTO = false;
             Main.instance = this;
             // this.websoketApi = '192.168.0.125:8082';
             // this.requestApi = 'http://192.168.0.125:8081';
             this.websoketApi = '132.232.34.32:8082';
             this.requestApi = 'http://132.232.34.32:8081';
-            this.resourseHttp='http://132.232.34.32/ttdx/';
+            this.IPArr = [
+                { isSelect: false, ip: '132.232.34.32' },
+                { isSelect: false, ip: '127.0.0.1' }
+            ];
+            // this.websoketApi = '127.0.0.1:8082';
+            // this.requestApi = 'http://127.0.0.1:8081';
+
+            this.resourseHttp = 'http://132.232.34.32/ttdx/';
             //手机信息
             this.phoneNews = {
                 statusHeight: 0,//手机系统栏的高度
                 deviceNews: '',//系统名称：Android / iOS
             };
             //是不是微信小游戏平台
-            this.wxGame=false;
+            this.wxGame = false;
             //用户信息
             // this.userInfo =this.wxGame?'1111':JSON.parse(localStorage.getItem("userInfo"));
-            this.userInfo =null;
+            this.userInfo = null;
             // this.$LOG('Main.js获取用户信息：', this.userInfo);
             //跳转划出界面标志
             this.sign = {
@@ -274,11 +281,11 @@
             this.gameView = {
                 desk_bg1: 'res/img/gameView/desk_bg1.png',
                 desk_bg2: 'res/img/gameView/desk_bg2.png',
-                wx_desk_bg1:'res/img/gameView/desk_bg1.jpg',
-                wx_desk_bg2:'res/img/gameView/desk_bg2.jpg'
+                wx_desk_bg1: 'res/img/gameView/desk_bg1.jpg',
+                wx_desk_bg2: 'res/img/gameView/desk_bg2.jpg'
             };
             //预加载图片数据
-            this.gameLoadImgArr=[
+            this.gameLoadImgArr = [
                 'res/img/common/login_bg.jpg',
                 'res/img/gameView/desk_bg1.png',
                 'res/img/gameView/desk_bg2.png',
@@ -464,7 +471,7 @@
                     node.top = node.top + this.phoneNews.statusHeight;
                 });
             }
-            if(this.wxGame){
+            if (this.wxGame) {
                 nodeArr.forEach(node => {
                     node.top = node.top + 30;
                 });
@@ -7455,6 +7462,114 @@
         }
     }
 
+    /**
+     * 打开弹框脚本
+     */
+    class OpenDiaLog extends Laya.Script {
+        onStart() {
+            Laya.stage.addChild(this.owner);
+            this.allowClickMaskClose = false;
+            this.dialog = this.owner.getChildByName('dialog1');
+            this.dialog.close();
+            this.diaLogMask = this.owner.getChildByName('diaLogMask');
+            this.diaLogMask.on(Laya.Event.CLICK, this, () => {
+                if (this.allowClickMaskClose) {
+                    this.close();
+                }
+            });
+        }
+        /**
+         * 打开弹框
+         * @param {*} JSthis 执行域
+         * @param {*} allowClickMaskClose 是否允许点击空白处关闭弹框
+         * @param {*} maskAlpha 遮罩的透明度
+         * @param {*} isShowCloseBtn 是否显示关闭按钮
+         * @param {*} closeFn 关闭回调
+         */
+        open(JSthis, allowClickMaskClose = false, maskAlpha = 0.2, isShowCloseBtn = true, closeFn, ctType = 1) {
+            this.closeCallBack = closeFn;
+            this.JSthis = JSthis;
+            this.owner.visible = true;
+            this.allowClickMaskClose = allowClickMaskClose;
+            this.diaLogMask.alpha = maskAlpha;
+            this.dialog._children.forEach(item => {
+                item.visible = false;
+            });
+            this.dialog.open();
+            let dialogBg = this.dialog.getChildByName('dialogBg' + ctType);
+            dialogBg.visible = true;
+            this.closeBtn = dialogBg.getChildByName('close');
+            this.closeBtn.visible = isShowCloseBtn;
+            this.closeBtn.on(Laya.Event.CLICK, this, () => {
+                if (this.closeBtn.visible) {
+                    this.close();
+                }
+            });
+        }
+        /**g关闭弹框 */
+        close() {
+            this.dialog.close();
+            this.owner.visible = false;
+            if (this.closeCallBack)
+                this.closeCallBack.call(this.JSthis);
+            this.closeBtn.off(Laya.Event.CLICK);
+        }
+    }
+
+    /**
+     * 设置IP
+     */
+    class SetIP{
+        init(thisJS){
+            this.num=0;
+           
+            this.logo=thisJS.owner.getChildByName('logo');
+            this.logo.on(Laya.Event.CLICK,this,()=>{
+                this.num++;
+                if(this.num%4==0){
+                    let myDiaLogJS = Laya.stage.getChildByName('dialogView').getComponent(OpenDiaLog);
+                    let dialogBg2 = myDiaLogJS.dialog.getChildByName('dialogBg2');
+                    this.listCt=dialogBg2.getChildByName('selectIPList');
+                    this.listCt.vScrollBarSkin = "";//运用滚动
+                    this.listCt.visible = true;
+                    let websoketApiIP=Main$1.websoketApi.split(':')[0];
+                    this.listCt.array = Main$1.IPArr;
+                    this.listCt.array.forEach(item=>{
+                        item.isSelect=item.ip==websoketApiIP?true:false;
+                    });
+                    this.listCt.renderHandler = new Laya.Handler(this, this.listCtOnRender);
+                    this.listCt.mouseHandler = new Laya.Handler(this, this.clicklistCtRow);
+                    myDiaLogJS.open(this, true, 0.3, true, null,2);
+                }
+            });
+        }
+
+        listCtOnRender(cell){
+            let IPBOX=cell.getChildByName('IP');
+            IPBOX.text=cell.dataSource.ip;
+            IPBOX.bgColor=cell.dataSource.isSelect?'#7CC9F7':'';
+        }
+
+        clicklistCtRow(Event){
+            if (Event.type == 'click') {
+                this.clearAllSelect();
+                let IPBOX = Event.target.getChildByName('IP');
+                IPBOX.bgColor='#7CC9F7';
+                Main$1.websoketApi=Event.target.dataSource.ip+':8082';
+                Main$1.requestApi='http://'+Event.target.dataSource.ip+':8081';
+                Main$1.$LOG( '选择的IP:',Main$1.websoketApi, Main$1.requestApi);
+            }
+        }
+
+        clearAllSelect() {
+            this.listCt.cells.forEach(item => {
+                let $IPBOX = item.getChildByName("IP");
+                $IPBOX.bgColor = null;
+            });
+        }
+    }
+    var setIP = new SetIP();
+
     class login extends Laya.Script {
         constructor() {
             super();
@@ -7478,6 +7593,7 @@
         onStart() {
             this.initOpenView();
             this.startLoadPage();
+            setIP.init(this);
             //微信小游戏背景图
             if (Main$1.wxGame)
                 this.initPage();
@@ -9034,54 +9150,6 @@
     }
 
     /**
-     * 打开弹框脚本
-     */
-    class OpenDiaLog extends Laya.Script{
-        onStart(){
-            Laya.stage.addChild(this.owner);
-            this.allowClickMaskClose=false;
-            this.dialog=this.owner.getChildByName('dialog1');
-            let dialogBg=this.dialog.getChildByName('dialogBg');
-            this.dialog.close();
-            this.diaLogMask=this.owner.getChildByName('diaLogMask');
-            this.diaLogMask.on(Laya.Event.CLICK,this,()=>{
-                if(this.allowClickMaskClose){
-                    this.close();
-                }
-            });
-            this.closeBtn=dialogBg.getChildByName('close');
-            this.closeBtn.on(Laya.Event.CLICK,this,()=>{
-                if(this.closeBtn.visible){
-                    this.close();
-                }
-            });
-        }
-        /**
-         * 打开弹框
-         * @param {*} JSthis 执行域
-         * @param {*} allowClickMaskClose 是否允许点击空白处关闭弹框
-         * @param {*} maskAlpha 遮罩的透明度
-         * @param {*} isShowCloseBtn 是否显示关闭按钮
-         * @param {*} closeFn 关闭回调
-         */
-        open(JSthis,allowClickMaskClose=false,maskAlpha=0.2,isShowCloseBtn=true,closeFn){
-            this.closeCallBack=closeFn;
-            this.JSthis=JSthis;
-            this.owner.visible=true;
-            this.closeBtn.visible=isShowCloseBtn;
-            this.allowClickMaskClose=allowClickMaskClose;
-            this.diaLogMask.alpha=maskAlpha;
-            this.dialog.open();
-        }
-        /**g关闭弹框 */
-        close(){
-            this.dialog.close();
-            this.owner.visible=false;
-            this.closeCallBack.call(this.JSthis);
-        }
-    }
-
-    /**
      * 该脚本为分享到微信好友或朋友圈的功能
      */
     class Share {
@@ -9625,14 +9693,15 @@
         qda(data, isUpdate, closeCallBack) {
             let myDiaLogJS = Laya.stage.getChildByName('dialogView').getComponent(OpenDiaLog);
             let dislog1 = myDiaLogJS.dialog;
+            let bg=dislog1.getChildByName('dialogBg1');
             //标题
-            let title = dislog1.getChildByName('dialogBg').getChildByName('title');
+            let title = bg.getChildByName('title');
             //内容
-            let content = dislog1.getChildByName('dialogBg').getChildByName('content');
+            let content = bg.getChildByName('content');
             //领取按钮
-            let btn1 = dislog1.getChildByName('dialogBg').getChildByName('btn1');
+            let btn1 = bg.getChildByName('btn1');
             //已经领取按钮
-            let btn0 = dislog1.getChildByName('dialogBg').getChildByName('btn0');
+            let btn0 = bg.getChildByName('btn0');
             title.text = data.name;
             content.text = data.detail;
             btn1.visible = data.giveNum > 0 ? false : true;
@@ -9644,7 +9713,7 @@
                 btn1.off(Laya.Event.CLICK);
                 if (closeCallBack)
                     closeCallBack();
-            });
+            },1);
         }
 
         /**
@@ -10262,8 +10331,8 @@
     		reg("game/pages/Set/Set.js",Set);
     		reg("game/common/MySwitch.js",MySwitch);
     		reg("game/pages/login/LoginUI.js",Login);
-    		reg("game/common/openView.js",openView);
     		reg("game/pages/login/Login.js",login);
+    		reg("game/common/openView.js",openView);
     		reg("game/pages/roomEnd/roomEndUI.js",RoomEndUI);
     		reg("game/pages/roomEnd/roomEnd.js",roomEnd);
     		reg("game/pages/paijuhuigu/paijuhuiguUI.js",paijuhuiguUI);
